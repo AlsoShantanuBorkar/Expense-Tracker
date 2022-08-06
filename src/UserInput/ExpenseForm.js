49394ae2a2fee0103b9e20bb3a4761388dba6e7c
Shantanu <sborkar9545@gmail.com>
2022-08-06 15:59:01 +0530

    Fixed ExpenseForm

--- src/UserInput/ExpenseForm.js
+++ src/UserInput/ExpenseForm.js
@@ -93,7 +93,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="button" onClick={props.cancelEditing}>cancel</button>
+        <button type="button" onClick={props.cancelEditing}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
